2196a088e8a2d5fcc14bf5b8fe3f4df285015cb1
Syed Sharook <syeds@oipinsurtech.com>
2026-01-13 00:16:35 +0530

    OOP Book Store

---
+++ book_store.py
@@ -0,0 +1,19 @@
+class Book:
+    def __init__(self,title,author,publication_year):
+        self.title = title
+        self.author = author
+        self.publication_year = publication_year
+
+    def get_age(self):
+        current_year = 2026
+        return current_year - self.publication_year
+    
+    def get_summary(self):
+        return f"Title: {self.title}, Author: {self.author}, Published: {self.publication_year}"
+
+
+Book1 = Book("OOPS Course", "Scott", 1998)
+Book2 = Book("Data Structures", "John", 2000)
+
+print("Book 1:", Book1.get_summary(), "AGE:", Book1.get_age())
+print("Book 2:", Book2.get_summary(), "AGE:", Book2.get_age())        
